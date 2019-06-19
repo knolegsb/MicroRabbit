2ef4a91b817c7559532c149ecd29e19a2ab0d059
knolegsb <seon2you@hotmail.com>
2019-06-19 09:55:44 -0700

    event publish

--- MicroRabbit.Banking.Application/Services/AccountService.cs
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -1,6 +1,10 @@
 ﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Application.Models;
+using MicroRabbit.Banking.Domain.Commands;
 using MicroRabbit.Banking.Domain.Interfaces;
 using MicroRabbit.Banking.Domain.Models;
+using MicroRabbit.Domain.Core.Bus;
+using MicroRabbit.Domain.Core.Events;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -10,15 +14,27 @@ namespace MicroRabbit.Banking.Application.Services
     public class AccountService : IAccountService
     {
         private readonly IAccountRepository _accountRepo;
+        private readonly IEventBus _bus;
 
-        public AccountService(IAccountRepository accountRepo)
+        public AccountService(IAccountRepository accountRepo, IEventBus bus)
         {
             _accountRepo = accountRepo;
+            _bus = bus;
         }
 
         public IEnumerable<Account> GetAccounts()
         {
             return _accountRepo.GetAccounts();
+        }
+
+        public void Transfer(AccountTransfer accountTransfer)
+        {
+            var createTransferCommand = new CreateTransferCommand(
+                    accountTransfer.FromAccount,
+                    accountTransfer.ToAccount,
+                    accountTransfer.TransferAmount
+                );
+            _bus.SendCommand(createTransferCommand);
         }
     }
 }
