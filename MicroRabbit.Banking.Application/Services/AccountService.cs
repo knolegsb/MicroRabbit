56eebd23703fd2151df246fa8b1c81a688da613d
knolegsb <seon2you@hotmail.com>
2019-06-19 01:43:03 -0700

    api setup

---
+++ MicroRabbit.Banking.Application/Services/AccountService.cs
@@ -0,0 +1,24 @@
+﻿using MicroRabbit.Banking.Application.Interfaces;
+using MicroRabbit.Banking.Domain.Interfaces;
+using MicroRabbit.Banking.Domain.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MicroRabbit.Banking.Application.Services
+{
+    public class AccountService : IAccountService
+    {
+        private readonly IAccountRepository _accountRepo;
+
+        public AccountService(IAccountRepository accountRepo)
+        {
+            _accountRepo = accountRepo;
+        }
+
+        public IEnumerable<Account> GetAccounts()
+        {
+            return _accountRepo.GetAccounts();
+        }
+    }
+}
---
+++ MicroRabbit.Banking.Data/Migrations/20190619083849_CorrectAccountBalance.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MicroRabbit.Banking.Data.Migrations
+{
+    public partial class CorrectAccountBalance : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccounBalance",
+                table: "Accounts",
+                newName: "AccountBalance");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "AccountBalance",
+                table: "Accounts",
+                newName: "AccounBalance");
+        }
+    }
+}
